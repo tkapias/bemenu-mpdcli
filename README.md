fffe69ae87904a1a0f558d761a9f313c9981010b
Tomasz Kapias <tomasz@tkapias.net>
2024-10-07 23:40:31 +0700

    init: v0.1

---
+++ README.md
@@ -0,0 +1,69 @@
+# bemenu-mpdcli
+
+A [bemenu](https://github.com/Cloudef/bemenu) client for [MPD](https://github.com/MusicPlayerDaemon/MPD) using [mpc](https://github.com/MusicPlayerDaemon/mpc). Control you local or remote Music Player Daemon from a simple and fast menu.
+
+![preview](assets/preview.gif)
+
+The preview uses my wrapper for bemenu: [bemenu-orange-wrapper](https://github.com/tkapias/bemenu-orange-wrapper).
+
+## Dependencies
+
+- [MPD](https://github.com/MusicPlayerDaemon/MPD)
+- [mpc](https://github.com/MusicPlayerDaemon/mpc)
+- [bemenu >= 0.6](https://github.com/Cloudef/bemenu)
+- [xclip](https://github.com/astrand/xclip)
+- [Nerd Fonts (Symbols Only)](https://github.com/ryanoasis/nerd-fonts/releases/latest)
+- Bash >= 5
+- Awk
+
+### Optional
+
+- [bemenu-orange-wrapper](https://github.com/tkapias/bemenu-orange-wrapper)
+
+## Installation
+
+```bash
+git clone --depth 0 https://github.com/tkapias/bemenu-mpdcli.git bemenu-mpdcli
+cd bemenu-mpdcli
+chmod +x bemenu-mpdcli.sh
+```
+
+- Optional: Symlink to a directory in your user's path:
+
+```bash
+ln -s $PWD/bemenu-mpdcli.sh $HOME/.local/bin/bemenu-mpdcli
+```
+
+## Features
+
+- Display current title, albumartist (or url/filename).
+- Player commands: Play/Pause, Stop, Prev, Next.
+- Queue:
+  - List position, title, albumartist, album (or url/filename).
+  - Play selected.
+- Playlists:
+  - List mpd playlists by filename.
+  - Replace queue with a playlist and play it.
+- Library:
+  - List albumartist.
+  - List albums for a selected albumartist.
+  - Replace queue with an album and play it.
+
+## Usage
+
+bemenu-mpdcli shoould work out of the box for a local mpd server.
+
+You can also provide arguments to mpc for custom locations.
+
+```bash
+bemenu-mpdcli [--host ip/socket] [--port num] [--partition name]
+```
+
+### Integrations
+
+- Binding example for bemenu-mpdcli in i3wm:
+
+```i3wm
+bindsym $mod+m exec --no-startup-id "bemenu-mpdcli"
+```
+
---
+++ bemenu-mpdcli.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+#########################
+# bemenu-mpdcli
+#
+# Licence: GNU GPLv3
+# Author: Tomasz Kapias
+#
+# Dependencies:
+#   bemenu v0.6.23
+#   bemenu-orange-wrapper
+#   Nerd-Fonts
+#   mpd
+#   mpc
+#   bash
+#   awk
+#
+#########################
+
+# ! arguments are transfered to mpc
+# Useful for --host,--port,--partition
+# See man mpc
+mpc=(mpc --quiet "$@")
+
+mode="queue"
+libmode="albumartist"
+albumartist=""
+
+header() {
+  local current
+  current="$("${mpc[@]}" current -f '[%title%|%file%][ (%albumartist%)]')"
+  [[ -n "$current" ]] && current=": $current"
+  echo -e "󰐎 Play/Pause$current\n󰓛 Stop\n󰒮 Previous\n󰒭 Next"
+  [[ "$mode" == "queue"     ]] && echo -e "󰲸 Playlists\n󰌱 Library"
+  [[ "$mode" == "playlists" ]] && echo -e "󰐑 Queue\n󰌱 Library"
+  [[ "$mode" == "library"   ]] && echo -e "󰐑 Queue\n󰲸 Playlists"
+}
+
+list() {
+  header
+  if [[ "$mode" == "queue" ]]; then
+    "${mpc[@]}" playlist -f '%position% [%title%|%file%][ (%albumartist% - %album%)]'
+  elif [[ "$mode" == "playlists" ]]; then
+    "${mpc[@]}" lsplaylists | awk 'NF' | sort -fu
+  elif [[ "$mode" == "library" ]] && [[ "$libmode" == "albumartist" ]]; then
+    "${mpc[@]}" list albumartist | awk 'NF' | sort -fu
+  elif [[ "$mode" == "library" ]] && [[ "$libmode" == "album" ]]; then
+    "${mpc[@]}" list album albumartist "$albumartist" | awk 'NF' | sort -fu
+  fi
+}
+
+list_action() {
+  if [[ "$mode" == "queue" ]]; then
+    "${mpc[@]}" play "${1%% *}"
+  elif [[ "$mode" == "playlists" ]]; then
+    "${mpc[@]}" clear
+    "${mpc[@]}" load "$1"
+    "${mpc[@]}" play
+    mode="queue"
+  elif [[ "$mode" == "library" ]] && [[ "$libmode" == "albumartist" ]]; then
+    libmode="album"
+    albumartist="$1"
+  elif [[ "$mode" == "library" ]] && [[ "$libmode" == "album" ]]; then
+    "${mpc[@]}" clear
+    "${mpc[@]}" find albumartist "$albumartist" album "$1" | sort | "${mpc[@]}" add
+    "${mpc[@]}" play
+    libmode="albumartist"
+    albumartist=""
+  fi
+}
+
+newmode() {
+  if [[ "$1" =~ ^󰐑|^󰐑 ]]; then
+    mode="queue"
+  elif [[ "$1" =~ ^󰲸|^󰲸 ]]; then
+    mode="playlists"
+  elif [[ "$1" =~ ^󰌱|^󰌱 ]]; then
+    mode="library"
+    libmode="albumartist"
+    albumartist=""
+  else
+    false
+  fi
+}
+
+operation() {
+  if [[ "$1" =~ ^󰐎 ]]; then
+    "${mpc[@]}" toggle
+  elif [[ "$1" =~ ^󰓛 ]]; then
+    "${mpc[@]}" stop
+  elif [[ "$1" =~ ^󰒮 ]]; then
+    "${mpc[@]}" prev
+  elif [[ "$1" =~ ^󰒭 ]]; then
+    "${mpc[@]}" next
+  else
+    false
+  fi
+}
+
+while
+  input=$( list | bemenu -p "󰎆 MPD ${mode^}$([[ $albumartist ]] && echo ": ${albumartist}")")
+    [[ -n "$input" ]]
+do
+    if newmode "$input"; then continue; fi
+    if operation "$input"; then continue; fi
+    list_action "$input"
+done
+
