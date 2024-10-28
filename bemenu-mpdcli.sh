835d19420523c8ba75096e100c7dfd66f0eb0f70
Tomasz Kapias <tomasz@tkapias.net>
2024-10-28 14:10:21 +0700

    fix: index after actions

--- bemenu-mpdcli.sh
+++ bemenu-mpdcli.sh
@@ -28,6 +28,8 @@ albumartist=""
 input=""
 
 # 5 first lines for commands/modes
+header_lines=5
+line_after_header=$(("$header_lines" + 1))
 header() {
   local current
   current="$("${mpc[@]}" current -f '[%title%|%file%][ (%albumartist%)]')"
@@ -56,6 +58,9 @@ list() {
 # contextual mpc commands on selected line (not in header)
 list_action() {
   if [[ "$mode" == "queue" ]]; then
+     local index
+     index=$(("$header_lines" + 1 + "${1%%[ 	]*}"))
+     bemenu+=("--index" "$index")
     "${mpc[@]}" play "${1%%[ 	]*}"
   elif [[ "$mode" == "playlists" ]]; then
     "${mpc[@]}" clear
@@ -65,13 +70,14 @@ list_action() {
   elif [[ "$mode" == "library" ]] && [[ "$libmode" == "albumartist" ]]; then
     libmode="album"
     albumartist="$1"
-    bemenu+=(--index 6)
+    bemenu+=("--index" "$line_after_header")
   elif [[ "$mode" == "library" ]] && [[ "$libmode" == "album" ]]; then
     "${mpc[@]}" clear
     "${mpc[@]}" find albumartist "$albumartist" album "$1" | sort | "${mpc[@]}" add
     "${mpc[@]}" play
     libmode="albumartist"
     albumartist=""
+    bemenu+=("--index" "4")
   fi
 }
 
@@ -79,12 +85,13 @@ list_action() {
 newmode() {
   if [[ "$1" =~ ^󰐑 ]]; then
     mode="queue"
+    bemenu+=("--index" "$line_after_header")
   elif [[ "$1" =~ ^󰲸 ]]; then
     mode="playlists"
-    bemenu+=(--index 6)
+    bemenu+=("--index" "$line_after_header")
   elif [[ "$1" =~ ^󰌱 ]]; then
     mode="library"
-    bemenu+=(--index 6)
+    bemenu+=("--index" "$line_after_header")
     libmode="albumartist"
     albumartist=""
   else
